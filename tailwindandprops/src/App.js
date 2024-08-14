25db24c18df5b305d4093238ba4615cf84151691
Sai <sai@gmail.com>
2024-08-14 08:51:23 -0400

    NewProject TailWind And Props

---
+++ tailwindandprops/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MyCard from './components/MyCard';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-center mt-3 bg-indigo-500 text-white' >TailWind And Props</h1>
+      <MyCard />
+    </>
+  );
+}
+
+export default App;
---
+++ tailwindandprops/src/components/MyCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NewCard from './NewCard'
+
+function MyCard() {
+    return (
+        <>
+            <NewCard product="Product Name" des="Product Description" price="$500.00" />
+        </>
+    )
+}
+
+export default MyCard
